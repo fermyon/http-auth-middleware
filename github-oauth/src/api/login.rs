748f463fbea3b7c8e3d42815a9794f4385d665d8
Kate Goldenring <kate.goldenring@fermyon.com>
2023-11-01 18:26:50 -0700

    fix(auth): Parse URL from request parts to enable wasmtime serve
    Signed-off-by: Kate Goldenring <kate.goldenring@fermyon.com>

--- github-oauth/src/api/login.rs
+++ github-oauth/src/api/login.rs
@@ -1,7 +1,7 @@
-use spin_sdk::http::{Headers, IncomingRequest, OutgoingResponse, ResponseOutparam};
+use spin_sdk::http::{Headers, OutgoingResponse, ResponseOutparam};
 
 /// `login` returns the login page.
-pub async fn login(_request: IncomingRequest, output: ResponseOutparam) {
+pub async fn login(output: ResponseOutparam) {
     const LOGIN_HTML: &[u8] = include_bytes!("../../login.html"); // TODO: this shouldn't be included statically.
 
     let headers = Headers::new(&[("content-type".to_string(), b"text/html".to_vec())]);
