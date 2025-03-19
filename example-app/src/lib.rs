ab8e48b42a11a22278bec29a95c04c0f15bfd9ff
itowlson <ivan.towlson@fermyon.com>
2025-03-19 13:42:30 +1300

    Use the pseudo-SDK helpers in the example back end
    Signed-off-by: itowlson <ivan.towlson@fermyon.com>

--- example-app/src/lib.rs
+++ example-app/src/lib.rs
@@ -9,27 +9,18 @@ wit_bindgen::generate!({
     generate_all,
 });
 
-use wasi::http::types::{Body, ErrorCode, Headers, Request, Response};
-use wit_bindgen_rt::async_support::futures::SinkExt;
+mod sdk;
+
+use wasi::http::types::{ErrorCode, Request, Response};
 
 struct ExampleApp;
 
 impl exports::wasi::http::handler::Guest for ExampleApp {
     async fn handle(_request: Request) -> Result<Response, ErrorCode> {
-        let (mut writer, reader) = wit_stream::new();
-        let (body, _efut) = Body::new(reader);
-
-        wit_bindgen_rt::async_support::spawn(async move {
-            writer
-                .send("Business logic executed!\n".into())
-                .await
-                .unwrap();
-        });
-
-        let headers = Headers::new();
-        headers.set("content-type", &["text/plain".into()]).unwrap();
-
-        Ok(Response::new(headers, Some(body)))
+        sdk::http::ResponseBuilder::new()
+            .with_status_code(200)
+            .with_header("content-type", "text/plain")?
+            .text("Business logic executed!\n")
     }
 }
 
